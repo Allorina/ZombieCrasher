14e336b7a5bdb29210b74413e32c6bfb8d9c1bce
Allorina <alinashve@gmail.com>
2023-07-27 23:19:56 +0200

    Obstacles

---
+++ Zombie Crasher/Assets/Scripts/Obstacle Scripts/DestroyAfterTime.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+
+    public float timer = 3f;
+
+    private void Start()
+    {
+        Invoke("DeactivateGameObject", timer); 
+    }
+
+    void DeactivateGameObject()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+}
